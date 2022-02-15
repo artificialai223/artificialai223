00b3036d8f8ebdccf05cc3628969d58271b45278
Henry Walter <airstrikeminecraft@gmail.com>
2022-02-15 22:08:36 +0000

    Update initserver.sh

--- initserver.sh
+++ initserver.sh
@@ -14,8 +14,6 @@ case ${answer:0:1} in
         docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:2.11.1 > /dev/null 2>&1
         echo "● Portainer agent installed."
         echo "● Use the server IP with the port 9001 to connect to this agent."
-        
-        portainer = true
     ;;
 esac
 
