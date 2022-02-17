50bf8ec440c9aa88c597ac231b7d3c48e96e439a
Henry Walter <airstrikeminecraft@gmail.com>
2022-02-17 18:15:21 +0000

    Update initserver.sh

--- initserver.sh
+++ initserver.sh
@@ -3,16 +3,19 @@
 read -p '● Hostname: ' hstnm
 
 sudo hostnamectl set-hostname hstnm
-
+clear
 read -p "● Install portainer agent? (y/n) " answer
+clear
 case ${answer:0:1} in
     y|Y )
         echo "● Installing docker..."
-        curl -sSL https://get.docker.com/ > /dev/null 2>&1 | CHANNEL=stable bash > /dev/null
+        curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+        clear
         echo "● Docker installed"
         echo "● Installing portainer agent..."
         
-        docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:2.11.1 > /dev/null
+        docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:2.11.1
+        clear
         echo "● Portainer agent installed."
         echo "● Use the server IP with the port 9001 to connect to this agent."
     ;;
@@ -27,5 +30,7 @@ sudo iptables -F
 
 iptables --flush
 
-echo "● IPTables cleared"
+clear
+
+echo "● IPTables records cleared"
 
