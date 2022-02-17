c9bfe0dc76ac5410262b7bbfd736010c0a6f1fd2
Henry Walter <airstrikeminecraft@gmail.com>
2022-02-17 18:12:30 +0000

    Update initserver.sh

--- initserver.sh
+++ initserver.sh
@@ -7,9 +7,12 @@ sudo hostnamectl set-hostname hstnm
 read -p "● Install portainer agent? (y/n) " answer
 case ${answer:0:1} in
     y|Y )
-        curl -sSL https://get.docker.com/ > /dev/null 2>&1 | CHANNEL=stable bash > /dev/null 2>&1
+        echo "● Installing docker..."
+        curl -sSL https://get.docker.com/ > /dev/null 2>&1 | CHANNEL=stable bash > /dev/null
+        echo "● Docker installed"
+        echo "● Installing portainer agent..."
         
-        docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:2.11.1 > /dev/null 2>&1
+        docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:2.11.1 > /dev/null
         echo "● Portainer agent installed."
         echo "● Use the server IP with the port 9001 to connect to this agent."
     ;;
